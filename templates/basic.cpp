cfb202d3591c8617ed20130ddf1bb7745e6d745b
root <root@BIBIMONI>
2023-09-24 00:13:53 +0700

    added contest 898 and edit templates

--- templates/basic.cpp
+++ templates/basic.cpp
@@ -2,7 +2,7 @@
  * File              : basic.cpp
  * Author            : distiled
  * Date              : 18.05.2023
- * Last Modified Date: 28.07.2023
+ * Last Modified Date: 24.09.2023
  * Last Modified By  : distiled
  */
 #include<bits/stdc++.h>
@@ -14,23 +14,21 @@ using namespace std;
 #define dbg(x...)
 #endif
 
-#define int           long long
-#define all(x)        x.begin(), x.end()
-#define rall(x)       x.rbegin(), x.rend()
-#define sz(x)         (int) x.size()
-#define __lcm(a, b)   (1ll * ((a) / __gcd((a), (b))) * (b))
-#define YES           cout << "YES\n";
-#define NO            cout << "NO\n";
-#define mod           1000000007
-#define endl          '\n'
-#define TLE           ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define MULTI         int t;cin>>t;while(t--)
-#define INF           (int) 1e18
-#define MAXN          (int) 1e6 + 5
-
-int nxt() {int n; cin >> n; return n;}
+#define int            long long
+#define pb             push_back()
+#define all(x)         x.begin(), x.end()
+#define sz(x)          (int) x.size()
+#define make_unique(x) sort(all(x)); x.erase(unique(all(x)), x.end());
+#define endl           '\n'
+#define MULTI          int t;cin>>t;while(t--)
+const int 
+      MAXN = (int) 1e6 + 5, 
+      INF = (int) 1e18, 
+      MOD = (int) 1e9 + 7; //998244353
+inline int nxt() { int n; cin >> n; return n; }
 
 signed main() {
-  TLE;
-
+  ios_base::sync_with_stdio(false); 
+  cin.tie(NULL);
+  
 }
